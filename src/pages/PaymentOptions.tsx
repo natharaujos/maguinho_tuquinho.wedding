8924ff85c7b672ca7424535c570738efbf59e5ce
Nathan Araujo <nathan.araujo@dtidigital.com.br>
2025-06-21 12:50:00 -0300

    adjustin navgation

--- src/pages/PaymentOptions.tsx
+++ src/pages/PaymentOptions.tsx
@@ -38,13 +38,15 @@ export default function PaymentOptions() {
       })
       const data = await res.json()
 
-      navigate('/pix-checkout', {
-        state: {
-          qrCode: data.qrCode,
-          initPoint: data.init_point,
-          docRefId,
-        },
-      })
+      // navigate('/pix-checkout', {
+      //   state: {
+      //     qrCode: data.qrCode,
+      //     initPoint: data.init_point,
+      //     docRefId,
+      //   },
+      // })
+
+      navigate(data.init_point)
     } catch (err) {
       console.error(err)
       alert('Erro ao gerar preferência PIX')
