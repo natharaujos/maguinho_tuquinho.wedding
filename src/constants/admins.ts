3ec2518e48f31c9eb7b4db6c29ab5cc08b5d3398
nathansaraujo191 <nathansaraujo191@gmail.com>
2025-08-12 19:33:39 -0300

    feat: adding new admin

--- src/constants/admins.ts
+++ src/constants/admins.ts
@@ -1,3 +1,7 @@
-const admins = ["nathansaraujo191@gmail.com", "magdasaraujo25@gmail.com"];
+const admins = [
+  "nathansaraujo191@gmail.com",
+  "magdasaraujo25@gmail.com",
+  "paulovictomartins4508@gmail.com",
+];
 
 export default admins;
