2be302ba93238220223bd56318d58f1d168e7530
nathansaraujo191 <nathansaraujo191@gmail.com>
2025-09-02 20:50:21 -0300

    feat: adding cursor pointer

--- src/components/ConfirmedGuests/index.tsx
+++ src/components/ConfirmedGuests/index.tsx
@@ -147,7 +147,7 @@ export function ConfirmedGuests() {
                   setSelectedGuestId(guest.id);
                   setConfirmOpen(true);
                 }}
-                className="absolute bottom-3 right-3 px-3 py-1 text-sm font-medium text-white bg-red-500 rounded-md shadow hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400 transition-colors duration-200"
+                className="absolute bottom-3 right-3 px-3 py-1 text-sm font-medium text-white bg-red-500 rounded-md shadow hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400 transition-colors duration-200 cursor-pointer"
               >
                 Remover
               </button>
